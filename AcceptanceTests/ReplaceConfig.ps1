513e787accd2a4ac02445b97f54500740c560314
Jakub Binkowski <jakubin@gmail.com>
2017-05-08 22:04:48 +0200

    Script to replace settings for TC.

---
+++ AcceptanceTests/ReplaceConfig.ps1
@@ -0,0 +1,13 @@
+﻿param(
+  [hashtable] $appSettings
+)
+
+$path = 'AcceptanceTests.dll.config'
+$config = [xml](Get-Content $path)
+
+foreach ($key in $appSettings.Keys) {
+    $node = $config.configuration.appSettings.add | where {$_.Key -eq $key}
+	$node.value = $appSettings.Item($key)
+}
+
+$config.Save($path)
