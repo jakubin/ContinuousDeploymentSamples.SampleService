c771aa3eaac5a5e1b6609f98b476d26fb3b30aba
Jakub Binkowski <jakubin@gmail.com>
2017-05-08 19:11:17 +0200

    Deployment tests.

---
+++ DeployTests/DeploymentTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using Shouldly;
+using Xunit;
+
+namespace DeployTests
+{
+    public class DeploymentTests
+    {
+        private SampleServiceClient _client;
+
+        public DeploymentTests()
+        {
+            _client = new SampleServiceClient(ConfigurationManager.AppSettings["ServiceUrl"]);
+        }
+
+        [Fact]
+        public void NotesApiWorks()
+        {
+            Should.NotThrow(() => _client.GetAllNotes());
+        }
+    }
+}
---
+++ DeployTests/SampleServiceClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DeployTests
+{
+    public class SampleServiceClient
+    {
+        private HttpClient _client;
+
+        public SampleServiceClient(string baseUrl)
+        {
+            _client = new HttpClient {BaseAddress = new Uri(baseUrl)};
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public List<NoteModel> GetAllNotes() => Get<List<NoteModel>>("/api/notes");
+
+        public void AddNote(NoteModel note)
+        {
+            var json = JsonConvert.SerializeObject(note);
+            var response = _client.PostAsync("/api/notes", new StringContent(json, Encoding.UTF8, "application/json")).Result;
+            response.EnsureSuccessStatusCode();
+        }
+
+        public void Reset()
+        {
+            _client.DeleteAsync("/api/testing").Result.EnsureSuccessStatusCode();
+        }
+
+        private T Get<T>(string path)
+        {
+            var json = _client.GetStringAsync(path).Result;
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+
+    public class NoteModel
+    {
+        public int? Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
